4728ec18426428fcdaacdc9feb9191a7160dbeee
Pun Butrach <pun.butrach@gmail.com>
2025-11-24 22:47:19 +0700

    fix: Use slightly older colour scheme for better contrast in dark mode
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/theme/color/tokens/ColorTokens.kt
+++ lawnchair/src/app/lawnchair/theme/color/tokens/ColorTokens.kt
@@ -102,7 +102,7 @@ object ColorTokens {
 
     @JvmField val DotColor = Accent3_200
 
-    @JvmField val FolderBackgroundColor = DayNightColorToken(Neutral1_50.setLStar(94.0), Neutral2_900.setLStar(12.0))
+    @JvmField val FolderBackgroundColor = DayNightColorToken(Neutral1_50.setLStar(94.0), Neutral2_50.setLStar(30.0))
 
     @JvmField val FolderIconBorderColor = ColorPrimary
 
