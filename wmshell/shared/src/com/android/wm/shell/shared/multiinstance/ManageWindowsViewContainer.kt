e6cb3e617bb9710e7920da05dc44408356be006e
Pun Butrach <pun.butrach@gmail.com>
2025-12-03 23:25:02 +0700

    fix: missing res and dependency on wmshell

--- wmshell/shared/src/com/android/wm/shell/shared/multiinstance/ManageWindowsViewContainer.kt
+++ wmshell/shared/src/com/android/wm/shell/shared/multiinstance/ManageWindowsViewContainer.kt
@@ -37,7 +37,9 @@ import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.window.TaskSnapshot
-import com.android.wm.shell.shared.R
+//import com.android.wm.shell.shared.R
+
+import com.android.wm.shell.R as SharedR
 
 /**
  * View for the All Windows menu option, used by both Desktop Windowing and Taskbar.
@@ -186,7 +188,7 @@ abstract class ManageWindowsViewContainer(
                 appSnapshotButton.cornerRadius = iconRadius
                 appSnapshotButton.setZOrderOnTop(true)
                 appSnapshotButton.contentDescription = context.resources.getString(
-                    R.string.manage_windows_icon_text, iconCount + 1
+                    SharedR.string.manage_windows_icon_text, iconCount + 1
                 )
                 appSnapshotButton.setOnClickListener {
                     onIconClickListener?.invoke(taskId)
