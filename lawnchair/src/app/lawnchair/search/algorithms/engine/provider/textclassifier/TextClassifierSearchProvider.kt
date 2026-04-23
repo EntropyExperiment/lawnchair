36e844b0e0400af945b460044b40c538ac742d2d
Pun Butrach <pun.butrach@gmail.com>
2026-04-24 01:31:31 +0700

    refactor: Slight cleanup
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/search/algorithms/engine/provider/textclassifier/TextClassifierSearchProvider.kt
+++ lawnchair/src/app/lawnchair/search/algorithms/engine/provider/textclassifier/TextClassifierSearchProvider.kt
@@ -8,6 +8,7 @@ import app.lawnchair.preferences.PreferenceManager
 import app.lawnchair.search.algorithms.engine.SearchProvider
 import app.lawnchair.search.algorithms.engine.SearchResult
 import com.android.launcher3.Utilities
+import kotlin.time.Duration.Companion.milliseconds
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,6 +16,8 @@ import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
 
 object TextClassifierSearchProvider : SearchProvider {
+    private const val TAG = "TextClassifierSearchProvider"
+
     /** Enables detailed logging, **Warning: Contain search query data** */
     private const val SENSITIVE_LOGGING = false
 
@@ -43,11 +46,11 @@ object TextClassifierSearchProvider : SearchProvider {
             try {
                 val request = TextClassification.Request.Builder(query, 0, query.length)
                     .build()
-                val classification = withTimeoutOrNull(CLASSIFICATION_TIMEOUT_MS) {
+                val classification = withTimeoutOrNull(CLASSIFICATION_TIMEOUT_MS.milliseconds) {
                     textClassifier.classifyText(request)
                 }
                 if (SENSITIVE_LOGGING) {
-                    Log.d("TextClassifierSearchProvider", "Classification result: $classification")
+                    Log.d(TAG, "Classification result: $classification")
                 }
                 if (classification?.actions?.isNotEmpty() == true) {
                     classification.actions.forEach { action ->
@@ -56,17 +59,13 @@ object TextClassifierSearchProvider : SearchProvider {
                                 title = action.title.toString(),
                                 subtitle = action.contentDescription.toString(),
                                 pendingIntent = action.actionIntent,
-                                icon = if (action.shouldShowIcon()) {
-                                    action.icon
-                                } else {
-                                    null
-                                },
+                                icon = if (action.shouldShowIcon()) action.icon else null,
                             ),
                         )
                     }
                 }
             } catch (e: Exception) {
-                Log.e("TextClassifierSearch", "Classification failed", e)
+                Log.e(TAG, "Classification failed", e)
             }
 
             searchResults
