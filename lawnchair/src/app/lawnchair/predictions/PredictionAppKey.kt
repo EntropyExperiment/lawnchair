4c76b96419150ef0a0e1242ba5a6d787487cdcea
Pun Butrach <pun.butrach@gmail.com>
2026-05-16 19:52:12 +0700

    fix: Commit that one file not tracked by git
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

---
+++ lawnchair/src/app/lawnchair/predictions/PredictionAppKey.kt
@@ -0,0 +1,35 @@
+package app.lawnchair.predictions
+
+import android.content.ComponentName
+
+object PredictionAppKey {
+    private const val DELIMITER = "/"
+
+    data class Parts(
+        val packageName: String,
+        val className: String,
+        val userToken: String,
+    )
+
+    fun create(componentName: ComponentName, userToken: String): String =
+        "${componentName.packageName}$DELIMITER${componentName.className}$DELIMITER$userToken"
+
+    fun create(componentName: ComponentName, userSerial: Long): String = create(componentName, userSerial.toString())
+
+    fun packageName(key: String): String = key.substringBefore(DELIMITER)
+
+    fun parse(key: String): Parts? {
+        val packageNameEnd = key.indexOf(DELIMITER)
+        val userTokenStart = key.lastIndexOf(DELIMITER)
+        if (packageNameEnd <= 0 || userTokenStart <= packageNameEnd + 1 || userTokenStart >= key.lastIndex) {
+            return null
+        }
+
+        return Parts(
+            packageName = key.substring(0, packageNameEnd),
+            className = key.substring(packageNameEnd + 1, userTokenStart),
+            userToken = key.substring(userTokenStart + 1),
+        )
+    }
+}
+
